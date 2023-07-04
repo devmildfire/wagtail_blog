c2025464da784f1482fcf799640d5f5503206946
mildfire <mildfire@gmail.com>
2023-07-04 21:51:57 +0500

    wagtail project start

---
+++ blog/blog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-_*_i125k&jz$=+9@*_=snsc2*dtco95uve4opf)gv$y_90)_%$"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
