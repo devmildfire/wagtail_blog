4abd30bfffd43e7f4f371f736e7ad28ef4226570
mildfire <mildfire@gmail.com>
2023-07-07 22:34:49 +0500

    tags added

--- blog/testblog/models.py
+++ blog/testblog/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 
+from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
 
 from wagtail.search import index
+
+
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'BlogPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE,
+    )
 
 
 class BlogIndexPage(Page):
@@ -14,10 +26,20 @@ class BlogIndexPage(Page):
         FieldPanel('intro')
     ]
 
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
+
+
 class BlogPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
+
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
 
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
@@ -25,6 +47,7 @@ class BlogPage(Page):
     ]
 
     content_panels = Page.content_panels + [
+        FieldPanel('tags'),
         FieldPanel('date'),
         FieldPanel('intro'),
         FieldPanel('body'),
